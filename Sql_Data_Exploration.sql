e34a5c0b96a33ccb615bbae5284a3a479d71930c
uday19web <94159829+uday19web@users.noreply.github.com>
2022-10-04 12:31:28 +0530

    Update Sql_Data_Exploration.sql

--- Sql_Data_Exploration.sql
+++ Sql_Data_Exploration.sql
@@ -1,1 +1,1 @@
-https://console.cloud.google.com/bigquery?sq=984477199947:4e9fbaa8e76d47e2987947d8efc66cff
+[https://console.cloud.google.com/bigquery?sq=984477199947:4e9fbaa8e76d47e2987947d8efc66cff]
